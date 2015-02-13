1eb38b8a7945af6a6f3cf466a0ba46ad4aa3cd89
chenbo <chenbo@bxd365.com>
2015-02-13 11:24:58 +0800

    add high light keys

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 if not CURSES then ui.set_theme('base16-monokai-dark') end
 require 'common'
 require 'fuzzy_finder'
+-- high light selected word
+keys[OSX and 'ch' or 'ch'] = textadept.editing.highlight_word
