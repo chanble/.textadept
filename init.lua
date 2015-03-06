bfe43a7a4a23ab47cee6a0f5dcbd15253d96f5dd
chenbo <chenbo@bxd365.com>
2015-03-06 17:02:32 +0800

    修改了空白符(whitespace)样式(style)

--- init.lua
+++ init.lua
@@ -3,3 +3,6 @@ require 'common'
 require 'fuzzy_finder'
 -- high light selected word
 keys[OSX and 'ch' or 'ch'] = textadept.editing.highlight_word
+
+buffer.set_whitespace_fore(buffer, 1, 0x45463b)
+
--- properties.lua
+++ properties.lua
@@ -1,6 +1,7 @@
 buffer.tab_width = 4
 buffer.use_tabs = true
 buffer.wrap_mode = buffer.WRAP_WORD
---buffer.view_ws = buffer.WS_VISIBLEALWAYS
+buffer.view_ws = buffer.WS_VISIBLEALWAYS
 buffer.virtual_space_options = buffer.VS_NONE
 buffer.wrap_visual_flags = buffer.WRAPVISUALFLAG_MARGIN
+buffer.whitespace_size = 2
