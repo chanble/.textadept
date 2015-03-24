b9efd1aed16cd3766b171bada3d859a892969ea8
chenbo <chenbo@bxd365.com>
2015-03-24 14:10:09 +0800

    change theme

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
-if not CURSES then ui.set_theme('base16-monokai-dark') end
+if not CURSES then ui.set_theme('cb-monokai-dark') end
 require 'common'
 require 'fuzzy_finder'
 -- high light selected word
 keys[OSX and 'ch' or 'ch'] = textadept.editing.highlight_word
-
-events.connect(events.VIEW_NEW, function()
-	local buffer = buffer
-	buffer.set_whitespace_fore(buffer, 1, 0x45463b)
-end)
