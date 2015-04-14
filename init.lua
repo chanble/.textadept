4ea4c8115d0750a60db9eb82f7dbd6f769b837b4
chenbo <chenbo@92wy.com>
2015-04-14 14:58:32 +0800

    update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 if not CURSES then ui.set_theme('base16-monokai-dark', {font = 'Consolas', fontsize = 12}) end
 require 'common'
-require 'fuzzy_finder'
+local fuzzy_finder = require 'fuzzy_finder'
+--fuzzy finder
+local filter = {
+	extensions = {'phar'}, --hidden files
+	folders = {"^%.", 'node_modules', 'vendor'} --hidden folders
+}
+keys[OSX and 'mp' or 'cp'] = {fuzzy_finder.show, filter}
 -- high light selected word
 keys[OSX and 'ch' or 'ch'] = textadept.editing.highlight_word
 events.connect(events.DOUBLE_CLICK, textadept.editing.highlight_word)
