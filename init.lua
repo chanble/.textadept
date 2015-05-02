096b7da3066f888507c25cd470cc4804fc145eeb
chenbo <chanbo.cn@gmail.com>
2015-05-02 13:15:21 +0800

    修改了fuzzy_finder

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
-if not CURSES then ui.set_theme('base16-monokai-dark', {font = 'Consolas', fontsize = 12}) end
+if not CURSES then ui.set_theme('base16-monokai-dark', {font = 'Consolas', fontsize = 13}) end
 require 'common'
 local fuzzy_finder = require 'fuzzy_finder'
 --fuzzy finder
 local filter = {
 	extensions = {'phar'}, --hidden files
-	folders = {"^%.", 'node_modules', 'vendor'} --hidden folders
+	folders = {'%.', 'node_modules', 'vendor'} --hidden folders
 }
 keys[OSX and 'mp' or 'cp'] = {fuzzy_finder.show, filter}
 -- high light selected word
