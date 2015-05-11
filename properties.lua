bbfdb3ed3c3c8aa750dbd4f91c0531402aa54a0c
chenbo <chanbo.cn@gmail.com>
2015-05-11 23:24:44 +0800

    add multi paste

--- properties.lua
+++ properties.lua
@@ -5,3 +5,4 @@ buffer.view_ws = buffer.WS_VISIBLEALWAYS
 buffer.virtual_space_options = buffer.VS_NONE
 buffer.wrap_visual_flags = buffer.WRAPVISUALFLAG_MARGIN
 buffer.whitespace_size = 2
+buffer.multi_paste = buffer.MULTIPASTE_EACH
